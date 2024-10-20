e41ab96922e5e4833129bad657a44c3d21b78bde
phuctranduy2 <100549068+phuctranduy2@users.noreply.github.com>
2024-10-20 16:19:17 +0700

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function colors {
+BOLD='\033[1m'
+YELLOW="\e[33m"
+BLACK="\e[30m"
+ENDCOLOR="\e[0m"
+}
+
+echo -e "\e[1;31m
+◤━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◥
+  Join Telegram: https://t.me/hiddengemnews
+   Follow  channel zero2hero | chuyên tool
+◣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◢
+      
+\e[0m"     
